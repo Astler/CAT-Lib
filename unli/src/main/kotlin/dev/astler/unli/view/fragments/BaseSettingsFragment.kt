13a8fe30f87e5a7faafeea9959059833cd2f8514
Astler <vladyclaw@gmail.com>
2020-03-23 11:46:33 +0200

    Prefs try

---
+++ unli/src/main/kotlin/dev/astler/unli/view/fragments/BaseSettingsFragment.kt
@@ -0,0 +1,14 @@
+package dev.astler.unli.view.fragments
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import dev.astler.unli.R
+
+class BaseSettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.prefs)
+
+    }
+
+}
