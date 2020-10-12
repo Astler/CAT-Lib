7c39479ea9fc998b0061506ec5fd9623961c1083
Astler <vladyclaw@gmail.com>
2020-10-12 12:49:32 +0300

    pro update

--- unli/src/main/kotlin/dev/astler/unli/utils/AdsUtils.kt
+++ unli/src/main/kotlin/dev/astler/unli/utils/AdsUtils.kt
@@ -4,5 +4,12 @@ import android.content.Context
 import dev.astler.unli.preferencesTool
 import java.util.*
 
-fun Context.canShowAds(): Boolean = preferencesTool.dayWithoutAds != GregorianCalendar.getInstance().get(
-    Calendar.DAY_OF_MONTH) && isOnline()
+fun Context.canShowAds(): Boolean {
+    return if (applicationContext.packageName.endsWith("paid")) {
+        false
+    }
+    else {
+        preferencesTool.dayWithoutAds != GregorianCalendar.getInstance().get(
+                Calendar.DAY_OF_MONTH) && isOnline()
+    }
+}
