1bd7b7363372c59707ac277edf3bc9a35e28000f
Astler <vladyclaw@gmail.com>
2020-12-14 06:52:35 +0200

    notifications start

--- unli/src/main/kotlin/dev/astler/unli/UnliApp.kt
+++ unli/src/main/kotlin/dev/astler/unli/UnliApp.kt
@@ -1,6 +1,9 @@
 package dev.astler.unli
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.multidex.MultiDexApplication
 import dev.astler.unli.utils.log
 
@@ -25,11 +28,27 @@ open class UnliApp : MultiDexApplication() {
         super.onCreate()
         prefs = PreferencesTool(this)
         applicationInstance = this
+        createNotificationChannels()
     }
+
+    open fun createNotificationChannels() {}
 
     fun initAppLanguage(context: Context) {
         AppSettings.loadLocale(
                 context
         )
+    }
+
+    private fun createNotificationChannel(pName: String = packageName, pDescription: String = "", pChannelId: String = "unli_default") {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(pChannelId, pName, importance).apply {
+                description = pDescription
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
