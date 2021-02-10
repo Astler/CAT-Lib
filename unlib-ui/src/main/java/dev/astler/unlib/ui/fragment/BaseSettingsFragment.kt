a23ebc908a990c4f60d7c70c6634b1cd590b0b4d
Astler <vladyclaw@gmail.com>
2021-02-10 09:54:03 +0200

    unlib start 13 (11/2/0)

---
+++ unlib-ui/src/main/java/dev/astler/unlib/ui/fragment/BaseSettingsFragment.kt
@@ -0,0 +1,32 @@
+package dev.astler.unlib.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import dev.astler.unlib.R
+import dev.astler.unlib.ui.interfaces.ActivityInterface
+
+open class BaseSettingsFragment : PreferenceFragmentCompat() {
+
+    lateinit var coreListener: ActivityInterface
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        coreListener = context as ActivityInterface
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.prefs)
+
+        findPreference<Preference>("dayWithoutAds")?.setOnPreferenceClickListener {
+            coreListener.showRewardAd()
+            true
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        coreListener.setToolbarTitle(getString(R.string.settings))
+    }
+}
---
+++ unlib-ui/src/main/java/dev/astler/unlib/ui/interfaces/ActivityInterface.kt
@@ -0,0 +1,14 @@
+package dev.astler.unlib.ui.interfaces
+
+import androidx.fragment.app.Fragment
+
+interface ActivityInterface {
+    fun backPressed(endAction: () -> Unit = {})
+    fun showInterstitialAd()
+    fun showRewardAd()
+    fun setCurrentFragment(fragment:Fragment)
+    fun setToolbarTitle(title: String)
+
+    fun setToolbarElevationEnabled(pElevationEnabled: Boolean) {}
+    fun toggleToolbar(pIsToolbarVisible: Boolean) {}
+}
