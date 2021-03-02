b31b522073866471dd93dfb6fb2a6ab3f64c0a54
Astler <vladyclaw@gmail.com>
2021-03-02 20:10:48 +0200

    splash fix

--- unlib-ui/src/main/java/dev/astler/unlib/ui/activity/SplashActivityUnLib.kt
+++ unlib-ui/src/main/java/dev/astler/unlib/ui/activity/SplashActivityUnLib.kt
@@ -3,21 +3,16 @@ package dev.astler.unlib.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dev.astler.unlib.utils.infoLog
 
 open class SplashActivityUnLib : AppCompatActivity() {
 
+    open var mPackageName: String = packageName
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        infoLog("Start ${javaClass.name} from $packageName", "ForAstler:UNLI")
         super.onCreate(savedInstanceState)
 
-        infoLog("start ${packageName}.MainActivity", "ForAstler:UNLI")
-        startActivity(Intent(Intent.ACTION_VIEW).setClassName(packageName, "${packageName}.MainActivity"))
-
-        infoLog("Perform splashTasks() in ${javaClass.name}", "ForAstler:UNLI")
+        startActivity(Intent(Intent.ACTION_VIEW).setClassName(mPackageName, "$mPackageName.MainActivity"))
         splashTasks()
-
-        infoLog("Finish ${javaClass.name}", "ForAstler:UNLI")
         finish()
     }
 
