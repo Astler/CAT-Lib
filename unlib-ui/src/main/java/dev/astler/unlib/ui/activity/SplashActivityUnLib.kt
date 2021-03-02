5f59b7c2309ad0a8a3aacc11abc1458b3d5de0e4
Astler <vladyclaw@gmail.com>
2021-03-02 20:46:55 +0200

    splash fix 3

--- unlib-ui/src/main/java/dev/astler/unlib/ui/activity/SplashActivityUnLib.kt
+++ unlib-ui/src/main/java/dev/astler/unlib/ui/activity/SplashActivityUnLib.kt
@@ -13,7 +13,7 @@ open class SplashActivityUnLib : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(Intent.ACTION_VIEW).setClassName(mPackageName, "$mPackageName.MainActivity"))
+        startActivity(Intent(Intent.ACTION_VIEW).setClassName(packageName, "$mPackageName.MainActivity"))
         splashTasks()
         finish()
     }
