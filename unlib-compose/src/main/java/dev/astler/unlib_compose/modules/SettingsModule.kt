2ff558a331d1ef7f70c622c7809f52867ad1592e
Astler <vladyclaw@gmail.com>
2021-11-06 11:01:58 +0200

    Compose Shit Start 1(0/1/0)

--- unlib-compose/src/main/java/dev/astler/unlib_compose/modules/SettingsModule.kt
+++ unlib-compose/src/main/java/dev/astler/unlib_compose/modules/SettingsModule.kt
@@ -2,18 +2,21 @@ package dev.astler.unlib_compose.modules
 
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.astler.unlib_compose.data.settings.Settings
 import dev.astler.unlib_compose.data.settings.SettingsImpl
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-abstract class SettingsModule {
-    @Binds
-    @Singleton
-    abstract fun bindUserSettings(
-        userSettingsImpl: SettingsImpl
-    ): Settings
-}
+//@Module
+//@InstallIn(SingletonComponent::class)
+//abstract class SettingsModule {
+//    @Binds
+//    @Singleton
+//    abstract fun bindUserSettings(
+//        userSettingsImpl: SettingsImpl
+//    ): Settings
+//}
