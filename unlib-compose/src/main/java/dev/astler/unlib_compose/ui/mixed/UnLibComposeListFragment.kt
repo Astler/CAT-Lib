ddbd562cf02a2b85495760bd5d55cb28275409e3
Astler <vladyclaw@gmail.com>
2021-11-07 12:29:44 +0200

    Compose Mixed Fragments 2.4 1(0/1/0)

--- unlib-compose/src/main/java/dev/astler/unlib_compose/ui/mixed/UnLibComposeListFragment.kt
+++ unlib-compose/src/main/java/dev/astler/unlib_compose/ui/mixed/UnLibComposeListFragment.kt
@@ -2,6 +2,7 @@ package dev.astler.unlib_compose.ui.mixed
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,13 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import dev.astler.unlib_compose.R
 
 @AndroidEntryPoint
 abstract class UnLibComposeListFragment<T> : UnLibComposeCoreFragment() {
     @Composable
-    open fun ItemsList(modifier: Modifier, pData: Array<T>?) {
+    open fun ItemsList(modifier: Modifier, contentPadding: PaddingValues = PaddingValues(0.dp), pData: Array<T>?) {
 
         if (pData == null) {
             LoadingScreen()
@@ -29,7 +31,7 @@ abstract class UnLibComposeListFragment<T> : UnLibComposeCoreFragment() {
             return
         }
 
-        LazyColumn(modifier) {
+        LazyColumn(modifier, contentPadding = contentPadding) {
             items(pData) { pItem ->
                 ContentItem(pItem)
             }
@@ -37,7 +39,8 @@ abstract class UnLibComposeListFragment<T> : UnLibComposeCoreFragment() {
     }
 
     @Composable
-    open fun ContentItem(pItem: T) {}
+    open fun ContentItem(pItem: T) {
+    }
 
     @Composable
     open fun LoadingScreen() {
