3cc3f3014d467596f12fe20343afca092adf2358
Vladislav Terentyev <v.terentyev@themindstudios.com>
2021-11-15 15:59:23 +0200

    Toasts Updated 2(0/2/0)

--- unlib-core/src/main/java/dev/astler/unlib/utils/ToastUtils.kt
+++ unlib-core/src/main/java/dev/astler/unlib/utils/ToastUtils.kt
@@ -2,10 +2,20 @@ package dev.astler.unlib.utils
 
 import android.content.Context
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import dev.astler.unlib.core.R
 
-fun Context.makeToast(pToastText: String = "Toast", pToastLength: Int = Toast.LENGTH_SHORT, pShowToast: Boolean = true, pToast: Toast? = null): Toast {
-    pToast?.cancel()
+var globalToast: Toast? = null
+const val cToastDefaultText = "Toast"
+
+fun Context.toast(
+    pToastText: CharSequence = cToastDefaultText,
+    pToastLength: Int = Toast.LENGTH_SHORT,
+    pShowToast: Boolean = true
+) {
+    globalToast?.cancel()
 
     val nToast = Toast.makeText(
         this,
@@ -16,20 +26,53 @@ fun Context.makeToast(pToastText: String = "Toast", pToastLength: Int = Toast.LE
     if (pShowToast)
         nToast.show()
 
-    return nToast
+    globalToast = nToast
 }
 
-fun Context.makeToast(pToastText: Int = R.string.app_name, pToastLength: Int = Toast.LENGTH_SHORT, pShowToast: Boolean = true, pToast: Toast? = null): Toast {
-    pToast?.cancel()
+fun Context.toast(
+    pToastRes: Int = R.string.app_name,
+    pToastLength: Int = Toast.LENGTH_SHORT,
+    pShowToast: Boolean = true
+) {
+    toast(resources.getText(pToastRes), pToastLength, pShowToast)
+}
 
-    val nToast = Toast.makeText(
-        this,
-        pToastText,
-        pToastLength
-    )
+fun Fragment.safeToast(
+    pToastRes: Int = R.string.app_name,
+    pToastLength: Int = Toast.LENGTH_SHORT,
+    pShowToast: Boolean = true
+) {
+    lifecycleScope.launchWhenResumed {
+        requireContext().toast(pToastRes, pToastLength, pShowToast)
+    }
+}
 
-    if (pShowToast)
-        nToast.show()
+fun Fragment.safeToast(
+    pToastText: CharSequence = cToastDefaultText,
+    pToastLength: Int = Toast.LENGTH_SHORT,
+    pShowToast: Boolean = true
+) {
+    lifecycleScope.launchWhenResumed {
+        requireContext().toast(pToastText, pToastLength, pShowToast)
+    }
+}
 
-    return nToast
+fun AppCompatActivity.safeToast(
+    pToastRes: Int = R.string.app_name,
+    pToastLength: Int = Toast.LENGTH_SHORT,
+    pShowToast: Boolean = true
+) {
+    lifecycleScope.launchWhenResumed {
+        toast(pToastRes, pToastLength, pShowToast)
+    }
+}
+
+fun AppCompatActivity.safeToast(
+    pToastText: CharSequence = cToastDefaultText,
+    pToastLength: Int = Toast.LENGTH_SHORT,
+    pShowToast: Boolean = true
+) {
+    lifecycleScope.launchWhenResumed {
+        toast(pToastText, pToastLength, pShowToast)
+    }
 }
