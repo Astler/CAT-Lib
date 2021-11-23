3b58b3bebecff006c41e24dd048ddcf60f37568f
Vladislav Terentyev <v.terentyev@themindstudios.com>
2021-11-24 01:19:34 +0200

    RV extensions 1(0/1/0)

--- unlib-ui/src/main/java/dev/astler/unlib/utils/views/RecyclerViewUtils.kt
+++ unlib-ui/src/main/java/dev/astler/unlib/utils/views/RecyclerViewUtils.kt
@@ -1,10 +1,14 @@
 package dev.astler.unlib.utils.views
 
 import android.content.Context
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dev.astler.unlib.view.MarginItemListDecorator
+import dev.astler.unlib.view.decorators.GridSpacingItemDecoration
+import dev.astler.unlib.view.decorators.MarginHorizontalListDecorator
 
 fun Context.getPossibleColumnsForScreenWight(imageWidth: Int = 50): Int {
     val displayMetrics = resources.displayMetrics
@@ -18,6 +22,63 @@ fun RecyclerView.setupCardList(pAdapter: RecyclerView.Adapter<out RecyclerView.V
     addItemDecoration(MarginItemListDecorator(22))
     adapter = pAdapter
     clipToPadding = false
+}
+
+fun RecyclerView.setupNestedGrid(pAdapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>, itemWidth: Int) {
+    ViewCompat.setNestedScrollingEnabled(this, false)
+
+    val nItems = context.getPossibleColumnsForScreenWight(itemWidth) - 1
+
+    val nGridManager = GridLayoutManager(context, nItems)
+
+    addItemDecoration(GridSpacingItemDecoration(nItems, 8, true))
+
+    nGridManager.isItemPrefetchEnabled = true
+    nGridManager.initialPrefetchItemCount = 10
+
+    layoutManager = nGridManager
+    setHasFixedSize(true)
+    setItemViewCacheSize(20)
+
+    adapter = pAdapter
+}
+
+fun RecyclerView.setupNestedList(pAdapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>, fixedSize: Boolean = true) {
+    ViewCompat.setNestedScrollingEnabled(this, false)
+
+    val nLinearLayoutManager = LinearLayoutManager(context)
+
+    nLinearLayoutManager.isItemPrefetchEnabled = true
+    nLinearLayoutManager.initialPrefetchItemCount = 5
+
+    addItemDecoration(MarginHorizontalListDecorator(8))
+
+    setHasFixedSize(fixedSize)
+    setItemViewCacheSize(20)
+
+    layoutManager = nLinearLayoutManager
+    adapter = pAdapter
+}
+
+fun RecyclerView.setupHorizontalScroll(pAdapter: RecyclerView.Adapter<out RecyclerView.ViewHolder>) {
+    ViewCompat.setNestedScrollingEnabled(this, false)
+
+    val nLinearLayoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+
+    nLinearLayoutManager.isItemPrefetchEnabled = true
+    nLinearLayoutManager.initialPrefetchItemCount = 5
+
+    setHasFixedSize(true)
+    setItemViewCacheSize(20)
+
+    clipToPadding = false
+
+    addItemDecoration(MarginHorizontalListDecorator(8))
+
+    isTransitionGroup = true
+
+    layoutManager = nLinearLayoutManager
+    adapter = pAdapter
 }
 
 fun RecyclerView.hideFABOnScroll(fab: FloatingActionButton) {
