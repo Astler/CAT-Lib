30fdfe528bc8c86d2efa0e1b901cd0f897128542
Vladislav Terentyev <v.terentyev@themindstudios.com>
2021-12-25 12:06:44 +0200

    Hilt Activity fix 1(0/1/0)

--- cat-ui/src/main/java/dev/astler/cat_ui/activities/CatHiltActivity.kt
+++ cat-ui/src/main/java/dev/astler/cat_ui/activities/CatHiltActivity.kt
@@ -1,5 +1,6 @@
 package dev.astler.cat_ui.activities
 
+import androidx.annotation.LayoutRes
 import androidx.viewbinding.ViewBinding
 
-abstract class CatHiltActivity<VB : ViewBinding> : CatActivity<VB>()
+abstract class CatHiltActivity<VB : ViewBinding>(@LayoutRes layoutId: Int) : CatActivity<VB>(layoutId)
