22b18ab76c2f0c69d4b7c74d8b733fed1b59fe48
Vladislav Terentyev <vladyclaw@gmail.com>
2023-12-13 00:16:01 +0200

    Updated libs

--- catlib-room/build.gradle.kts
+++ catlib-room/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    val roomVersion = "2.6.0"
+    val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.room:room-paging:$roomVersion")
