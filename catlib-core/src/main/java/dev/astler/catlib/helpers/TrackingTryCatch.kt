9870ee06697ddbf4c9a23dc4e6e1cc4cecd9c81c
Vladislav Terentyev <vladyclaw@gmail.com>
2023-12-15 01:17:04 +0200

    Updated trackedTry for improve usability

--- catlib-core/src/main/java/dev/astler/catlib/helpers/TrackingTryCatch.kt
+++ catlib-core/src/main/java/dev/astler/catlib/helpers/TrackingTryCatch.kt
@@ -7,21 +7,18 @@ import com.google.firebase.ktx.Firebase
  * try-catch-finally extensions with ability to use default return value and errors tracking
  */
 
-fun trackedTry(errorCatchAction: (() -> Unit)? = null, finallyAction: (() -> Unit)? = null, action: (() -> Unit)? = null) {
+fun trackedTry(errorCatchAction: ((Exception) -> Unit)? = null, action: (() -> Unit)? = null) {
     try {
         action?.invoke()
     } catch (e: Exception) {
-        errorCatchAction?.invoke()
+        errorCatchAction?.invoke(e)
         Firebase.crashlytics.recordException(e)
         errorLog("Exception START! ${e.message}\n\n${e.stackTraceToString()}\n\n${e.cause}\n\nException END!")
-    } finally {
-        finallyAction?.invoke()
     }
 }
 
 fun <T> trackedTry(
-    errorCatchAction: (() -> T)? = null,
-    finallyAction: (() -> T)? = null,
+    errorCatchAction: ((Exception) -> T)? = null,
     fallbackValue: T,
     action: (() -> T)? = null
 ): T {
@@ -30,8 +27,6 @@ fun <T> trackedTry(
     } catch (e: Exception) {
         Firebase.crashlytics.recordException(e)
         errorLog("Exception START! ${e.message}\n\n${e.stackTraceToString()}\n\n${e.cause}\n\nException END!")
-        errorCatchAction?.invoke() ?: fallbackValue
-    } finally {
-        finallyAction?.invoke() ?: fallbackValue
+        errorCatchAction?.invoke(e) ?: fallbackValue
     }
 }
