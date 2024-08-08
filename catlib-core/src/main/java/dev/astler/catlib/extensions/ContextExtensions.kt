89e81419459c9a0004dd892da836d391f4e98250
Astler <vladyclaw@gmail.com>
2024-08-09 02:48:23 +0300

    Added getStringByName for core module #2

--- catlib-core/src/main/java/dev/astler/catlib/extensions/ContextExtensions.kt
+++ catlib-core/src/main/java/dev/astler/catlib/extensions/ContextExtensions.kt
@@ -1,8 +1,15 @@
 package dev.astler.catlib.extensions
 
+import android.annotation.SuppressLint
 import android.content.Context
 
-fun Context.getStringByName(string: String): Int {
+@SuppressLint("DiscouragedApi")
+fun Context.getStringIdByName(string: String): Int {
     val id = resources.getIdentifier(string, "string", packageName)
     return if (id != 0) id else gg.pressf.resources.R.string.nothing
+}
+
+fun Context.getStringByName(name: String, fallbackPrefix: String = "no:"): String {
+    val id = getStringIdByName(name)
+    return if (id != 0) getString(id) else fallbackPrefix + name
 }
